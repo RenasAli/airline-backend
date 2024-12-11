4f7ad72958c97317b1b2f94edd1f1debd1a3d974
August Dedenroth <augu0577@stud.kea.dk>
2024-12-11 15:25:19 +0100

    Add Departure/Arrival time to mapping

--- backend/Config/MappingProfile.cs
+++ backend/Config/MappingProfile.cs
@@ -20,15 +20,15 @@ namespace backend.Config
 
 			CreateMap<User, JwtRequest>();
 
-            CreateMap<Airport, AirportResponse>();
+			CreateMap<Airport, AirportResponse>();
 
-            CreateMap<Airplane, AirplaneResponse>();
-            
-            CreateMap<Airline, AirlineResponse>();
+			CreateMap<Airplane, AirplaneResponse>();
+			
+			CreateMap<Airline, AirlineResponse>();
 
-            CreateMap<BookingCreationRequest, BookingProcessedRequest>();
+			CreateMap<BookingCreationRequest, BookingProcessedRequest>();
 
-            CreateMap<TicketCreationRequest, TicketProcessedRequest>();
+			CreateMap<TicketCreationRequest, TicketProcessedRequest>();
 
 			CreateMap<Booking, BookingResponse>();
 			
@@ -38,12 +38,14 @@ namespace backend.Config
 				.ForMember(dest => dest.ArrivalPortName, opt => opt.MapFrom(src => src.Flight.ArrivalPortNavigation.Name))
 				.ForMember(dest => dest.FlightClassName, opt => opt.MapFrom(src => src.FlightClass.Name))
 				.ForMember(dest => dest.FlightTravelTime, opt => opt.MapFrom(src => src.Flight.TravelTime))
+				.ForMember(dest => dest.FlightDepartureTime, opt => opt.MapFrom(src => src.Flight.DepartureTime))
+				.ForMember(dest => dest.FlightCompletionTime, opt => opt.MapFrom(src => src.Flight.CompletionTime))
 				.ForMember(dest => dest.PassengerFirstName, opt => opt.MapFrom(src => src.Passenger.FirstName))
 				.ForMember(dest => dest.PassengerLastName, opt => opt.MapFrom(src => src.Passenger.LastName))
 				.ForMember(dest => dest.PassengerEmail, opt => opt.MapFrom(src => src.Passenger.Email));
 
-        	CreateMap<Booking, BookingResponse>()
-            	.ForMember(dest => dest.Tickets, opt => opt.MapFrom(src => src.Tickets));
-        }
-    }
+			CreateMap<Booking, BookingResponse>()
+				.ForMember(dest => dest.Tickets, opt => opt.MapFrom(src => src.Tickets));
+		}
+	}
 }
--- backend/Dtos/TicketResponse.cs
+++ backend/Dtos/TicketResponse.cs
@@ -4,14 +4,16 @@ public class TicketResponse {
 
 //    public int Id {get; set;}
 
-    public decimal Price {get; set;}
-    public string TicketNumber {get; set;}
-    public string FlightCode { get; set; } = null!;
-    public string DeparturePortName { get; set; } = null!;
-    public string ArrivalPortName { get; set; } = null!;
-    public string FlightClassName { get; set; } = null!;
-    public int FlightTravelTime { get; set; }
-    public string PassengerFirstName { get; set; } = null!;
-    public string PassengerLastName { get; set; } = null!;
-    public string PassengerEmail { get; set; } = null!;
+	public decimal Price {get; set;}
+	public string TicketNumber {get; set;}
+	public string FlightCode { get; set; } = null!;
+	public string DeparturePortName { get; set; } = null!;
+	public string ArrivalPortName { get; set; } = null!;
+	public string FlightClassName { get; set; } = null!;
+	public int FlightTravelTime { get; set; }
+	public DateTime FlightDepartureTime { get; set; }
+	public DateTime FlightCompletionTime { get; set; }
+	public string PassengerFirstName { get; set; } = null!;
+	public string PassengerLastName { get; set; } = null!;
+	public string PassengerEmail { get; set; } = null!;
 }
